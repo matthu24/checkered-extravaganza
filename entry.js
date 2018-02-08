955a0721198a3bfe6d1615ac32ffa11f225494ea
Matthew Hu <matthu24@gmail.com>
2018-02-07 20:15:26 -0800

    Add click on square

--- entry.js
+++ entry.js
@@ -5,9 +5,15 @@ function setupBoard(){
   let square;
   for (var i = 0; i < 64; i++) {
     square = document.createElement('li');
-    // square.innerHTML = '';
+    square.innerHTML = '';
     board.append(square);
     //add a dark square every other, but we need to switch every row too
+    // 0-7   Math.floor(i/8) % 2 == 0
+    // 8-15
+    // 16-23
+    // 24-31
+    // 32-39
+    // 40-47
     if (i % 2 == 0 && Math.floor(i/8) % 2 === 1) {
       square.classList.add('dark-square')
     }
@@ -19,10 +25,11 @@ function setupBoard(){
 
 setupBoard();
 
-//
-// 0-7   Math.floor(i/8) % 2 == 0
-// 8-15
-// 16-23
-// 24-31
-// 32-39
-// 40-47
+let board = document.getElementById('board');
+
+board.addEventListener('click', function(e){
+  //the LI needs to be in caps otherwise doen't work
+  if (e.target && e.target.nodeName=='LI') {
+    e.target.classList.add('red-square')
+  }
+});
