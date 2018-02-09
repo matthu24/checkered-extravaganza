c326225b04dd7c57a2164f0dfac965988fd6d545
Matthew Hu <matthu24@gmail.com>
2018-02-08 21:21:59 -0800

    Add start and stop game

--- entry.js
+++ entry.js
@@ -1,11 +1,15 @@
 import Board from './board';
 
+setupBoard();
+let counter = 0;
+
+
 function setupBoard(){
   let board = document.getElementById('board');
   let square;
   for (var i = 0; i < 64; i++) {
     square = document.createElement('li');
-    square.innerHTML = '';
+    square.className = 'black-square';
     board.append(square);
     //add a dark square every other, but we need to switch every row too
     // 0-7   Math.floor(i/8) % 2 == 0
@@ -14,39 +18,108 @@ function setupBoard(){
     // 24-31
     // 32-39
     // 40-47
-    if (i % 2 == 0 && Math.floor(i/8) % 2 === 1) {
-      square.classList.add('dark-square')
-    }
-    if(i % 2 == 1 && Math.floor(i/8) % 2 === 0){
-      square.classList.add('dark-square')
-    }
+    // if (i % 2 == 0 && Math.floor(i/8) % 2 === 1) {
+    //   square.classList.add('dark-square')
+    // }
+    // if(i % 2 == 1 && Math.floor(i/8) % 2 === 0){
+    //   square.classList.add('dark-square')
+    // }
     // random is either one or zero
   }
 }
 
-setupBoard();
+
+
+let start = document.getElementById('start-game');
+start.addEventListener('click',function(){
+  counter = 0;
+  let score = document.getElementById('score')
+  score.innerHTML = "Score: " + counter;
+  timer();
+  var ul = document.getElementById("board");
+  //items is a list of sorts
+  var items = ul.getElementsByTagName("li");
+  for (var i = 0; i < items.length; ++i) {
+    // do something with items[i], which is a <li> element
+    blink(items[i])
+  }
+
+});
 
 let board = document.getElementById('board');
-
 board.addEventListener('click', function(e){
   //the LI needs to be in caps otherwise doen't work
   if (e.target && e.target.nodeName=='LI') {
-    e.target.classList.add('red-square')
+    if (e.target.className === 'red-square') {
+      counter+=2
+    }else{
+      counter-=1
+    }
+    e.target.className = 'bold'
+    console.log(counter)
+
+    score.innerHTML = "Score: " + counter;
+    // console.log("hello")
   }
 });
 
 
-let random;
-let test = document.getElementById('test')
-setInterval(function(){
-  random = Math.floor(Math.random()*2)
-  // console.log(random)
-  if (random === 0) {
-    // test.classList.add('dark-square')
-    test.className = 'dark-square'
-  }else{
-    // test.classList.add('red-square')
-    test.className = 'red-square'
 
-  }
-},500)
+
+
+
+
+//blinks for 15 seconds
+function blink(square){
+  let random;
+  let interval = setInterval(function(){
+    random = Math.floor(Math.random()*17)
+    // console.log(random)
+    if(random > 4){
+      square.className = 'black-square'
+    }
+    else if(random === 4){
+      square.className = 'yellow-square'
+    }else if(random === 3){
+      square.className = 'green-square'
+    }else if (random === 2) {
+      // square.classList.add('dark-square')
+      square.className = 'purple-square'
+    }else if (random === 1){
+      // square.classList.add('red-square')
+      square.className = 'red-square'
+    }else if (random === 0){
+      square.className = 'blue-square'
+    }
+  },700)
+
+  setTimeout(function(){
+    clearInterval(interval)
+    var ul = document.getElementById("board");
+    var items = ul.getElementsByTagName("li");
+    for (var i = 0; i < items.length; ++i) {
+      // do something with items[i], which is a <li> element
+      items[i].className='black-square';
+    }
+  },15000)
+}
+
+
+function timer(){
+  let remaining = 15;
+  let timer = document.getElementById('timer')
+  let interval = setInterval(function(){
+    remaining-=1;
+    timer.innerHTML = remaining;
+  },1000)
+  setTimeout(function(){
+    clearInterval(interval);
+  },15000)
+}
+
+
+
+
+
+// let element = document.getElementById('test')
+// test(element);
