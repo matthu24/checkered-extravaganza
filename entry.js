b4b193336a0225ce9ddc5e6fd89f88b5213b900e
Matthew Hu <matthu24@gmail.com>
2018-02-08 22:47:45 -0800

    cleanup

--- entry.js
+++ entry.js
@@ -3,7 +3,6 @@ import Board from './board';
 setupBoard();
 let counter = 0;
 
-
 function setupBoard(){
   let board = document.getElementById('board');
   let square;
@@ -16,23 +15,8 @@ function setupBoard(){
       square.className = 'black-square'
     }
     board.append(square);
-    //add a dark square every other, but we need to switch every row too
-    // 0-7   Math.floor(i/8) % 2 == 0
-    // 8-20
-    // 16-23
-    // 24-31
-    // 32-39
-    // 40-47
-    // if (i % 2 == 0 && Math.floor(i/8) % 2 === 1) {
-    //   square.className = 'dark-square'
-    // }
-    // if(i % 2 == 1 && Math.floor(i/8) % 2 === 0){
-    //   square.className = 'dark-square'
-    // }
-    // random is either one or zero
   }
 }
-
 
 
 let start = document.getElementById('start-game');
@@ -48,7 +32,6 @@ start.addEventListener('click',function(){
     // do something with items[i], which is a <li> element
     blink(items[i])
   }
-
 });
 
 let board = document.getElementById('board');
@@ -64,15 +47,8 @@ board.addEventListener('click', function(e){
     console.log(counter)
 
     score.innerHTML = "Score: " + counter;
-    // console.log("hello")
   }
 });
-
-
-
-
-
-
 
 //blinks for 20 seconds
 function blink(square){
@@ -97,25 +73,16 @@ function blink(square){
       square.className = 'blue-square'
     }
   },800)
-
   setTimeout(function(){
     clearInterval(interval)
     var ul = document.getElementById("board");
     var items = ul.getElementsByTagName("li");
     for (var i = 0; i < 64; ++i) {
-      // if (i % 2 == 0 && Math.floor(i/8) % 2 === 1) {
-      //   items[i].className = 'black-square'
-      // }
-      // if(i % 2 == 1 && Math.floor(i/8) % 2 === 0){
-      //   items[i].className = 'black-square'
-      // }else{
-      //   items[i].className = 'white-square';
-      // }
-      // do something with items[i], which is a <li> element
       items[i].className='black-square';
     }
   },20000)
 }
+
 
 
 function timer(){
@@ -129,10 +96,3 @@ function timer(){
     clearInterval(interval);
   },20000)
 }
-
-
-
-
-
-// let element = document.getElementById('test')
-// test(element);
